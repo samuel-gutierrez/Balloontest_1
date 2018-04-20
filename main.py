3a6c5354e61d7369d302a3ff5367421816e1edec
Samuel Gutierrez R <samuel.gutierrez@usach.cl>
2018-04-20 15:03:17 -0300

    First steps.

---
+++ main.py
@@ -0,0 +1,17 @@
+# INFO:
+# 1.- Main program for balloon test.
+#
+
+# IDEA:
+# 1.- Cada cierto tiempo toma fotos.
+# 2.- Analiza la foto en fn del peso.
+# 3.- Si cumple requisitos (liviana) la manda.
+# 4.- Pasa la foto al programa del ST.
+# 5.- Guarda la foto y los resultados.
+# 6.- Repeat.
+
+# 1.- Python imports.
+
+# 2.- Take picture.
+
+# El problema aqui es q no se q tiempo de exposicion darle... podria partir con 100 ms hasta 1 s.
